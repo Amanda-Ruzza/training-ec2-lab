375bf65f7abbcc9420cb1963688650174a272196
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-03-29 16:51:26 -0400

    Added 3 EC2 instances to the private subnet

--- security-groups.tf
+++ security-groups.tf
@@ -33,5 +33,42 @@ resource "aws_security_group" "my_public_app_sg" {
     protocol    = "-1" # TCP + UDP
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+#resources for the EC2 instances on the private subnets
+resource "aws_security_group" "my_private_app_sg" {
+  name        = "my_private_app_sg"
+  description = "Allow access to this server"
+  vpc_id      = data.aws_vpc.main_vpc.id
+
+
+
+
+  # INBONUD CONNECTIONS
+
+  ingress {
+    description = "Allow SSH into the EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["68.160.218.208/32"] # using my ip as an experiment
+  }
+
+  # ingress {
+  # description = "Allow HTTP into the EC2"
+  # from_port = 80
+  # to_port = 80
+  # protocol = "tcp"
+  # cidr_blocks = ["0.0.0.0/0"] #only for my own IP address
+
+
+  #OUTBOUND CONNECTIONS
+
+  egress {
+    description = "Allow access to the world"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # TCP + UDP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
